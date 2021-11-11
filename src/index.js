250ec9420851467d3b40fe1044c46b8d1a3b5614
ghodrat keshtmand <60603913+ImAWinner1@users.noreply.github.com>
2021-11-11 09:43:00 +0330

    initialFunction

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+function App(props){
+    return(
+        <button onClick={alert('Hello you!')}>
+            click me!
+        </button>
+    )
+}
+ReactDom.render(
+    <App />,
+    document.getElementById('root')
+)
